bc365453b4d8f2b5740a789abcb5ca9097aa979a
Brian Li <antai0926@gmail.com>
2020-07-23 10:32:05 +0800

    Updates

---
+++ precache-manifest.a720242b2e7ccec6451dee4e82446a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b4c35af06ba66af3f95a7fdd46c9d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "acede9dcc4861a6107bb",
+    "url": "/monsters-rolodex/static/css/main.a7a6e81c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acede9dcc4861a6107bb",
+    "url": "/monsters-rolodex/static/js/main.58607aa6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
